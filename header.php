8282e0ffe2b6f96f1e59018d41683a84b242a744
kywleon <66321555+kywleon@users.noreply.github.com>
2020-12-12 00:22:43 +0800

    Testing for edit branch
    add the "Hello Here i make the edit features" in .nav-item a

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Hello Here i make the edit features</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
